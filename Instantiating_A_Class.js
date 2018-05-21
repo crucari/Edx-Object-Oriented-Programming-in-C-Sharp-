2ccbbb7a7b5342373b75c627688943b6e61c9b72
Carina Cruz <lecruz@bu.edu>
2018-05-21 23:07:05 +0000

    Instantiating a class

---
+++ Instantiating_A_Class.js
@@ -0,0 +1,12 @@
+// Instantiating a Class
+DrinksMachine dm = new DrinksMachine
+
+// Instantiating a Class by Using Type Inference
+var dm = new DrinksMachine();
+
+// Using Object Members
+var dm = new DrinksMachine();
+dm.Make = "Fourth Coffee";
+dm.Model = "Beancrusher 3000";
+dm.Age = 2;
+dm.MakeEspresso();
