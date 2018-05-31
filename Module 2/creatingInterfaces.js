2fdc1fa3ad32c3d97e31732cec2a71be08e8ecd0
Carina Cruz <lecruz@bu.edu>
2018-05-31 00:04:30 +0000

    interface update

--- Module 2/creatingInterfaces.js
+++ Module 2/creatingInterfaces.js
@@ -3,3 +3,12 @@ public interface IBeverage
 {
   // Methods, properties, events, and indexers go here.
 }
+
+The following example shows an interface that defines one read-only property and two methods:
+// Defining an Interface
+public interface ILoyaltyCardHolder
+{
+   int TotalPoints { get; }
+   int AddPoints(decimal transactionValue);
+   void ResetPoints();
+}
